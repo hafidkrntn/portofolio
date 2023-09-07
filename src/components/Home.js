51d9f18df968130188881213822dd1ab43e8251b
hafidkrntn <hafidkrntn@gmail.com>
2023-09-07 13:13:01 +0700

    kedua commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,21 @@
-import React from 'react'
-import { HiArrowNarrowRight } from 'react-icons/hi'
+import React from "react";
+import Photo from "../assets/photoprofil.png";
 
 const Home = () => {
-    return (
-        <div name="home" className="w-full h-screen bg-[#30475E]">
-            
-            {/* Container */}
-            <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-                <p className="text-red-600">Hi, my name is</p>
-                <h1 className="text-4xl sm:text-3xl font-bold text-[#F5F5F5]">Muhamad Hafid Kurnianton</h1>
-                <h2 className="text-4xl sm:text-2xl font-bold text-[#121212]">I'm an enthusiastic coding & technology</h2>
-                <div>
-                    <button className="text-white border-2 px-6 py-3 mt-2 flex items-center hover:bg-red-600 hover:border-red-600">
-                        View More 
-                    <span className="group-hover:rotate-90 duration-300">
-                    <HiArrowNarrowRight className="ml-3" />
-                    </span>
-                    </button>
-                </div>
-            </div>
-
+  return (
+    <div name="home" className="container mx-auto h-screen">
+      <div className="flex justify-between mx-auto">
+        <div className="my-auto">
+            <h1 className="mb-2 text-2xl">Hello Iam</h1>
+            <h1 className="text-6xl">Muhamad Hafid Kurnianton</h1>
+            <h1 className="text-4xl mt-2">Website Developer</h1>
         </div>
-    )
-}
+        <div>
+            <img src={Photo} alt="ini foto" className="mx-auto"/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav)
 
     return(
-        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#30475E] text-gray-300'>
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 text-black'>
             <div>
                 <img src={Logo} alt="LogoImage" style={{width: '120px'}} />
             </div>
